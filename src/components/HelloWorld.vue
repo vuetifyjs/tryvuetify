0706832bb80df1b736cb73487235aa49d821daae
John Leider <john@vuetifyjs.com>
2022-10-18 09:40:35 -0500

    refactor(HelloWorld): update page design

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <v-container class="fill-height">
     <v-responsive class="d-flex align-center text-center fill-height">
       <h1 class="d-inline-block text-h2 font-weight-black">
-        <span class="font-weight-thin text-h2">try</span><span class="text-primary">vuetify</span>
+        <span class="font-weight-thin text-h2">try</span><span class="text-primary">Vuetify</span>
       </h1>
 
       <div class="py-4" />
@@ -15,38 +15,62 @@
         </template>
 
         <template #append>
-          <v-btn icon="mdi-content-copy" border variant="text">
-            <v-icon color="grey" />
+          <v-btn
+            :icon="copied ? 'mdi-check' : 'mdi-content-copy'"
+            border
+            variant="text"
+            @click="copy"
+          >
+            <v-fade-transition leave-absolute>
+              <v-icon
+                :key="copied"
+                color="grey"
+              />
+            </v-fade-transition>
           </v-btn>
         </template>
       </v-card>
 
-      <div class="py-12" />
-
-      <v-row class="d-flex align-center justify-center">
-        <v-col cols="auto">
-          <v-btn
-            append-icon="mdi-open-in-new"
-            color="purple"
-            href="https://next.vuetifyjs.com/introduction/why-vuetify/#feature-guides"
-            size="small"
-            target="_blank"
-            variant="outlined"
-          >
-            Full Guide
-          </v-btn>
-        </v-col>
-      </v-row>
+      <div class="py-3">
+        <v-btn
+          href="https://next.vuetifyjs.com/getting-started/installation/"
+          target="_blank"
+          rel="noopener noreferrer"
+          append-icon="mdi-open-in-new"
+        >
+          Full Guide
+        </v-btn>
+      </div>
     </v-responsive>
   </v-container>
 
   <v-footer app>
-    <span class="text-caption ml-auto text-primary">
-      What is Vuetify?
-    </span>
+    <a
+      class="d-inline-block mx-auto"
+      href="http://vuetifyjs.com/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <v-img
+        width="48"
+        src="https://cdn.vuetifyjs.com/docs/images/logos/v.svg"
+      />
+    </a>
   </v-footer>
 </template>
 
 <script setup>
+  import { ref } from 'vue'
 
+  const copied = ref(false)
+
+  function copy () {
+    copied.value = true
+
+    navigator.clipboard.writeText('vue create my-app -p vuetifyjs/v3-preset')
+
+    setTimeout(() => {
+      copied.value = false
+    }, 2000)
+  }
 </script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -14,6 +14,10 @@ import { createVuetify } from 'vuetify'
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   defaults: {
+    VBtn: {
+      size: 'small',
+      variant: 'text',
+    },
     VCard: {
       elevation: 20,
       rounded: 'lg',
